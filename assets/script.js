fb17ec79ae7ee51e3f3ff23724701b85666882d6
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-08-05 12:07:24 -0400

    worked on html and css and added local storage for subscribe section

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@
 
 var openLibraryApi = "http://openlibrary.org/search.json?q="
 var googleBooksApi = "https://books.googleapis.com/books/v1/volumes?q="
-var googleSearchApi = "https://books.googleapis.com/books/v1/volumes?q="+document.getElementById("input").value+"&key=AIzaSyB0jD4ojzXkBjv5QQueyFNrhDdBgI0Azqc"
+var googleSearchApi = "https://books.googleapis.com/books/v1/volumes?q=" + document.getElementById("input").value + "&key=AIzaSyB0jD4ojzXkBjv5QQueyFNrhDdBgI0Azqc"
 var testApi = "https://books.googleapis.com/books/v1/volumes?q=love&key=AIzaSyB0jD4ojzXkBjv5QQueyFNrhDdBgI0Azqc"
 console.log(testApi)
 
@@ -32,18 +32,18 @@ console.log(testApi)
 //     } );
 // }
 
-function getAuthor(){
-    document.getElementById('output2').innerHTML= "";
-    fetch("https://books.googleapis.com/books/v1/volumes?q="+document.getElementById("input2").value+"&key=AIzaSyB0jD4ojzXkBjv5QQueyFNrhDdBgI0Azqc")
-    .then(a=> a.json())//converts the response which is currently a string into an object we can then read
-    .then(response =>{
-        // for(var i = 0; i < response.items.length;i++){
-        //     document.getElementById("output").innerHTML +="<h2>" + response.items[i].volumeInfo.title + "</h2>"+response.items[i].volumeInfo.authors+" <br> <img src='http://books.google.com/books/content?id=" +response.items[i].id+ "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'><br>";
-        // }
-        for(var i = 0; i < 15;i++){
-            document.getElementById("output2").innerHTML +="<h2>" + response.items[i].volumeInfo.authors + "</h2><br> <img src='http://books.google.com/books/content?id=" +response.items[i].id+ "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'><br>" + response.items[i].volumeInfo.description;
-        }
-    } );
+function getAuthor() {
+    document.getElementById('output2').innerHTML = "";
+    fetch("https://books.googleapis.com/books/v1/volumes?q=" + document.getElementById("input2").value + "&key=AIzaSyB0jD4ojzXkBjv5QQueyFNrhDdBgI0Azqc")
+        .then(a => a.json())//converts the response which is currently a string into an object we can then read
+        .then(response => {
+            // for(var i = 0; i < response.items.length;i++){
+            //     document.getElementById("output").innerHTML +="<h2>" + response.items[i].volumeInfo.title + "</h2>"+response.items[i].volumeInfo.authors+" <br> <img src='http://books.google.com/books/content?id=" +response.items[i].id+ "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'><br>";
+            // }
+            for (var i = 0; i < 15; i++) {
+                document.getElementById("output2").innerHTML += "<h2>" + response.items[i].volumeInfo.authors + "</h2><br> <img src='http://books.google.com/books/content?id=" + response.items[i].id + "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'><br>" + response.items[i].volumeInfo.description;
+            }
+        });
 }
 
 function getBooks() {
@@ -58,8 +58,14 @@ function getBooks() {
                 console.log(response.items[i].saleInfo)
                 document.getElementById("output").innerHTML += "<h2>" + response.items[i].volumeInfo.title + "</h2>" +
                     response.items[i].volumeInfo.authors + " <br> " +
-                    '<a href="'+response.items[i].saleInfo.buyLink +'" target=blank>'+
-                    " <img src='http://books.google.com/books/content?id=" + response.items[i].id + "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api' >"+ "</a>"+" <br>" + response.items[i].volumeInfo.description;
+                    '<a href="' + response.items[i].saleInfo.buyLink + '" target=blank>' +
+                    " <img src='http://books.google.com/books/content?id=" + response.items[i].id + "&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api' >" + "</a>" + " <br>" + response.items[i].volumeInfo.description;
             }
         });
 }
+
+//local storage for subscribing
+function store(){
+    var inputEmail= document.getElementById("email");
+    localStorage.setItem("email", inputEmail.value);
+   };
